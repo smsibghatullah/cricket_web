1c3f3373b7470532165c3aa1f56e41ec597f1a38
smsibghatullah <smsibghatullah@gmail.com>
2023-04-14 11:12:03 +0500

    fullscorecard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,7 @@ use App\Item;
 use App\Models\Player;
 use App\Models\Tournament;
 use App\Models\Fixture;
+use App\Models\FixtureScore;
 use App\Models\Team;
 
 class HomeController extends Controller
@@ -39,8 +40,8 @@ class HomeController extends Controller
         $tournament = Tournament::query();
         $tournament = $tournament->orderBy('id')->get();
         $match_results = Fixture::query();
-        $match_results->where('running_inning','=',3)->orderBy('id')->get();;
-        // $match_results = $match_results
+        $match_results->where('running_inning','=',3);
+        $match_results = $match_results->orderBy('id')->get();
         $teams = Team::query()->get()->pluck(
           'name',
           'id'
@@ -53,7 +54,18 @@ class HomeController extends Controller
     {
 
         $result = [];
-        return view('search_player',compact('result'));
+
+        $card =FixtureScore::Where('fixture_id','=',$id)
+                ->selectRaw("sum(runs) as total_runs")
+                ->selectRaw("count(isfour) as total_fours")
+                ->selectRaw("count(issix) as total_six")
+                ->selectRaw("playerId")
+                ->groupBy('playerId')
+                ->get();
+
+
+        // dd($card);
+        return view('score_card',compact('result'));
 
     }
 
