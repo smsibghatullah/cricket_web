b3c6987b04f7a51059f82852d01500dde8277b9f
smsibghatullah <smsibghatullah@gmail.com>
2023-04-14 09:38:04 +0500

    results show on home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,8 @@ use App\Item;
 
 use App\Models\Player;
 use App\Models\Tournament;
-
+use App\Models\Fixture;
+use App\Models\Team;
 
 class HomeController extends Controller
 
@@ -37,13 +38,28 @@ class HomeController extends Controller
     {
         $tournament = Tournament::query();
         $tournament = $tournament->orderBy('id')->get();
-        return view('home',compact('tournament'));
+        $match_results = Fixture::query();
+        $match_results->where('running_inning','=',3)->orderBy('id')->get();;
+        // $match_results = $match_results
+        $teams = Team::query()->get()->pluck(
+          'name',
+          'id'
+        );
+
+        return view('home',compact('tournament', 'match_results','teams'));
+    }
+
+    public function fullScorecard(int $id)
+    {
+
+        $result = [];
+        return view('search_player',compact('result'));
+
     }
 
     public function search_player()
     {
 
-        // return view('search_player');
         $result = [];
         return view('search_player',compact('result'));
 
